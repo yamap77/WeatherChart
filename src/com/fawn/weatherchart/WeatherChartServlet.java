8394a23ae9e3d176c2e1806738db818f67c4a1ce
yamap77 <tiejiazhao@gmail.com>
2013-07-08 16:55:33 -0400

    request to servlet for data

--- src/com/fawn/weatherchart/WeatherChartServlet.java
+++ src/com/fawn/weatherchart/WeatherChartServlet.java
@@ -1,13 +1,42 @@
 package com.fawn.weatherchart;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+/*
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+*/
 @SuppressWarnings("serial")
 public class WeatherChartServlet extends HttpServlet {
+	
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+      response.setContentType("application/json;charset=utf-8");
+      String station=request.getParameter("county");
+      /*
+      JSONObject json=new JSONObject();
+      JSONArray array=new JSONArray();
+      JSONObject member=new JSONObject();
+      member.put("arrayData", sampleData);
+      array.add(member);
+      json.put("jsonArray", array);
+      */
+      String json="{\"station\":[{\"fawn\":[1,-1,4,3,2,-1,3],\"nws\":[2,3,4,2,3,4,0,-1]},{\"fawn\":[1,-1,-3,3,2,-1,3],\"nws\":[2,3,4,2,3,4,5,-1]}]}";
+  // String Alachua = "{\"fawn\":[1,-1,4,3,2,-1,3],\"nws\":[2,3,4,2,3,4,0,-1]}";
+  //    String Apopka="{\"fawn\":[1,-1,-3,3,2,-1,3],\"nws\":[2,3,4,2,3,4,5,-1]}";
+      PrintWriter pw=response.getWriter();
+      
+      pw.print(json);
+      pw.close();
+	}
 }
+
